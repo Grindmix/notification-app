53dd4e6302db9ed0be5180e919407ebe33d13263
Grindmix <71901283+Grindmix@users.noreply.github.com>
2022-06-19 21:04:03 +0300

    Update README.md

--- notify_app/notify/urls.py
+++ notify_app/notify/urls.py
@@ -4,5 +4,5 @@ from notify import views
 urlpatterns = [
     path('notifications/', views.notification_list),
     path('notifications/send/<int:pk>/', views.notification_send),
-    path('notification/<int:pk>/', views.notification_detail),
+    path('notifications/<int:pk>/', views.notification_detail),
 ]
